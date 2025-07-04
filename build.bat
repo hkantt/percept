88a83d1c0cba5c3f7567f11b706ca987a0eca9f3
hkantt <197491524+hkantt@users.noreply.github.com>
2025-07-04 12:52:02 +0530

    Add files via upload

--- build.bat
+++ build.bat
@@ -3,9 +3,7 @@
 cmake -S . -B build
 cmake --build build --config Release
 
-REM Get timestamp
 for /f %%a in ('wmic os get localdatetime ^| find "."') do set dt=%%a
-rem dt format: YYYYMMDDHHMMSS.milliseconds+timezone
 
 set year=%dt:~2,2%
 set month=%dt:~4,2%
@@ -16,11 +14,9 @@ set second=%dt:~12,2%
 
 set TIMESTAMP=%day%%month%%year%_%hour%%minute%%second%
 
-REM Copy to target directory
 echo Creating release percept.pyd in release ...
 copy /Y "build\Release\percept.pyd" "release\percept.pyd"
 
-REM Copy to backup with timestamp
 echo Creating backup percept_%TIMESTAMP%.pyd in backups ...
 copy /Y "build\Release\percept.pyd" "backups\percept_%TIMESTAMP%.pyd"
 exit /b
